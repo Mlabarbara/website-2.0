c38972a02c7f2ecff399872c92faeeb6922e4b1b
Mark LaBarbara <mlabarbara@gmail.com>
2024-12-13 13:47:06 -0700

    create tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+    "./src/**/*.{html,js,css}",
+  ];
+  export const theme = {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  };
+  export const plugins = [];
+  export const darkMode = 'class';
+  
